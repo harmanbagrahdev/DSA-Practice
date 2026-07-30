502c25d1829e4150c8a49b41a76157f759036e5f
Harman Bagrah <harmanbagrah.dev@gmail.com>
2026-07-30 20:55:29 +0530

    Day 23 of DSA -- Tried another approach of Problem (Move zeroes to end)

--- Arrays/Move-zeroes-to-end.cpp
+++ Arrays/Move-zeroes-to-end.cpp
@@ -53,6 +53,34 @@ void moveZeroesOptimal(vector<int>& nums) {
     for(auto i : nums) cout << i << " ";
 }
 
+
+// Leet code Solution -- Optimal
+
+// T = O(n)
+// S = O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) return;
+        for(int i = j + 1; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+
 int main() {
     vector<int> nums = {0,1,2,0,2,5,0,5,7,8,0,4,0,7,1,2,4};
 
