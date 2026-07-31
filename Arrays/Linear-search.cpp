a7ccc2fc090bb64819cfa92bd088eeff0855554a
Harman Bagrah <harmanbagrah.dev@gmail.com>
2026-07-31 22:22:51 +0530

    Day 24 of DSA -- Solved problem --> union of two sorted arrays and learned linear search

--- Arrays/Linear-search.cpp
+++ Arrays/Linear-search.cpp
@@ -1,8 +1,25 @@
-// Linear Search
-
 #include <bits/stdc++.h>
 using namespace std;
 
+// Linear Search
+
+int linearSearch(vector<int>& arr, int n) {
+  for(int i = 0; i < arr.size(); i++) {
+    if(arr[i] == n) {
+      return i;
+      break;
+    }
+  }
+  return -1;
+}
+
 int main() {
-    
+    vector<int> arr = {0,1,2,3,4,5,10,12,45,1,20,5};
+    int n;
+    cout << "Enter number to find : ";
+    cin >> n;
+
+    cout << "Searching index of " << n << " in array : ";
+    cout << linearSearch(arr, n);
+    cout << endl;
 }
--- Arrays/Union-of-two-sorted-arrays.cpp
+++ Arrays/Union-of-two-sorted-arrays.cpp
@@ -1,1 +1,92 @@
-// implement it by tomorrow 
+#include <bits/stdc++.h>
+using namespace std;
+
+// brute force -- use a set and take union of two arrays in that set and then make that set a vector
+
+// T = O(n1.log(n) + n2.log(n)) + log(n1 + n2)
+// S = O(n1 +n2) + O(n1 +n2)
+void unionArr(vector<int>& a, vector<int>& b) {
+  set<int> s;
+  // T = O(n1.log(n)) , where n and n1 --> size of set and size of array
+  // S = O(n1)
+  for(auto num : a) s.insert(num);
+  
+  // T = O(n2.log(n))
+  // S = O(n2)
+  for(auto num : b) s.insert(num);
+  
+  // T = O(n1 + n2)
+  // S = O(n1 + n2)
+  vector<int>(s.begin(), s.end());
+  
+  // S = O(n1 + n2) --> to display the answer
+  for(auto i : s) {
+    cout << i << " ";
+  } cout << endl;
+}
+
+
+// optimal -- two pointer method
+
+// T = O(n + m)
+// S = O(n1 + n2) --> for returning answer only 
+// auxiliary space --> S = O(1)
+vector<int> unionArray(vector<int>& a, vector<int>& b) {
+  int n = a.size();
+  int m = b.size();
+  int i = 0;
+  int j = 0;
+  vector<int> unionArr;
+
+  while(i < n && j < m) {
+    if(a[i] <= b[j]) {
+      if(unionArr.empty() || unionArr.back() != a[i]) {
+        unionArr.push_back(a[i]);
+      }
+      if(a[i] == b[j]) {
+        j++;
+      }
+      
+      i++;
+    }
+
+    else{
+      if(unionArr.empty() || unionArr.back() != b[j]) {
+        unionArr.push_back(b[j]);
+      }
+      j++;
+    }
+  }
+
+  while(j < m) {
+    if(unionArr.empty() || unionArr.back() != b[j]) {
+      unionArr.push_back(b[j]);
+    }
+    j++;
+  }
+
+  while(i < n) {
+    if(unionArr.empty() || unionArr.back() != a[i]) {
+      unionArr.push_back(a[i]);
+    }
+    i++;
+  }
+
+  return unionArr;
+}
+
+
+int main() {
+  vector<int> a = {1,2,3,4,5};
+  vector<int> b = {4,5,6,7,8,9};
+
+  // unionArr(a, b);
+
+  vector<int> arr = unionArray(a, b);
+
+  for(auto i : arr) {
+    cout << i << " ";
+  }
+  cout << endl;
+  // cout << "Program completed\n";
+}
