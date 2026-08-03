0ca5a6221402e82d446062d0cbedf4f5ec324fe6
Harman Bagrah <harmanbagrah.dev@gmail.com>
2026-08-03 19:56:52 +0530

    Day 26 of DSA -- optimal and better solution of number that appears once (last problem)

--- Arrays/find-number-that-appears-once-and-other-number-twice.cpp
+++ Arrays/find-number-that-appears-once-and-other-number-twice.cpp
@@ -32,9 +32,56 @@ int appearedOnce(vector<int>& arr, int n) {
   return -1;
 }
 
+
+// better solution : By using hashing
+// hashing can be done in two different ways :
+// array hashing (not applicable if the array contains negatives or very large numbers)
+// Hashing using the map data structure
+
+// T = O()
+// S = O()
+int appearedOnceBetter(vector<int>& arr, int n) {
+  int maxi = arr[0]; // initialise
+  
+  // find maximum element
+  for(int i = 0; i < n; i++) {
+    maxi = max(maxi, arr[i]);  
+  }
+  
+  // count frequency
+  unordered_map<long, int> mpp(maxi + 1);
+  for(int j = 0; j < n; j++) {
+    mpp[arr[j]]++;
+  }
+
+  // return whose frequency is one
+  for(int i = 0; i < n; i++) {
+    if(mpp[arr[i]] == 1) return arr[i];
+  }
+
+  return -1;
+}
+
+// optimal solution
+// T = O(n)
+// S = O(1)
+int appearedOnceOptimal(vector<int>& arr, int n) {
+  int xorr = 0;
+
+  for(int i = 0; i < n; i++) {
+    xorr = xorr ^ arr[i];
+  }
+
+  return xorr;
+}
+
 int main() {
   vector<int> arr = {1,1,2,2,3,3,4,4,5,6,6};
   int n = arr.size();
 
-  cout << appearedOnce(arr, n) << endl;
+  // cout << appearedOnce(arr, n) << endl;
+
+  // cout << appearedOnceBetter(arr, n) << endl;
+
+  cout << appearedOnceOptimal(arr, n) << endl;
 }
