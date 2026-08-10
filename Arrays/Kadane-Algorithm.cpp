6a449bc4d1cab985d582dc471d55b59ebd7fe747
Harman Bagrah <harmanbagrah.dev@gmail.com>
2026-08-11 00:49:04 +0530

    Day 33 of DSA -- Completed and revised Kadane's algorithm problem

--- Arrays/Kadane-Algorithm.cpp
+++ Arrays/Kadane-Algorithm.cpp
@@ -29,10 +29,70 @@ int largestSum(vector<int>& arr) {
   return maxi;
 }
 
+// Better solution
+// T = O(n^2)
+// S = O(1)
+int largestSumBetter(vector<int>& arr) {
+  int n = arr.size();
+  int maxi = INT16_MIN;
+  int start, end;
+
+  for(int i = 0; i < n; i++) {
+    start = i;
+    int sum = 0;
+
+    for(int j = i; j < n; j++) {
+      sum += arr[j];
+      maxi = max(maxi, sum);
+    }
+  }
+
+  return maxi;
+}
+
+// Optimal solution
+// T = O(n)
+// S = O(1)
+int largestSumOptimal(vector<int>& arr) {
+  int n = arr.size();
+  int maxi = INT16_MIN;
+  int sum = 0;
+
+  int start = 0, end = 0;
+  int ansStart, ansEnd;
+
+  for(int i = 0; i < n; i++) {
+    if(sum == 0) start = i; // if we need to print the subarray
+
+    sum += arr[i];
+    if(sum > maxi) {
+      ansStart = start, ansEnd = i; // if we need to print the subarray
+    }
+    
+    maxi = max(maxi, sum);
+
+    if(sum < 0) sum = 0;
+  }
+
+  if(sum < 0) maxi = 0; // incase there is no subarray with sum > 0 then return 0
+
+  // incase there is no subarray with sum > 0 then return 0
+  for(int i = ansStart; i <= ansEnd; i++) {
+    cout << arr[i] << " ";
+  } cout << endl;
+
+  return maxi;
+}
+
 int main() {
   vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
 
-  cout << largestSum(arr) << endl;
+  // cout << largestSum(arr) << endl;
+
+  // cout << largestSumBetter(arr) << endl;
+
+  cout << largestSumOptimal(arr) << endl;
+
   cout << "completed\n";
 }
