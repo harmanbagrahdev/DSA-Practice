969675d9679d0c43635bf4338ea26c3862e28edc
Harman Bagrah <harmanbagrah.dev@gmail.com>
2026-09-07 23:15:50 +0530

    Day 57 of DSA --- optimized problem : maximum product subarray in array

--- Arrays/Maximum-product-subarray-in-an-array.cpp
+++ Arrays/Maximum-product-subarray-in-an-array.cpp
@@ -20,8 +20,30 @@ int maxProductBrute(vector<int>& nums) {
   return maxProd;
 }
 
-int main() {
-  vector<int> nums = {1,2,3,4,5,0};
+// Optimal solution 1
+// T = O(n)
+// S = O(1)
+int maxProductOptimal_1(vector<int>& nums) {
+  int n = nums.size();
+  int product1 = 1;
+  int product2 = 1;
+  int ans = INT16_MIN;
 
-  cout << maxProductBrute(nums) << endl;
+  for(int i = 0; i < n; i++) {
+    if(product1 == 0) product1 = 1;
+    if(product2 == 0) product2 = 1;
+    product1 *= nums[i];
+    product2 *= nums[(n-1) - i];
+
+    ans = max(ans, max(product1, product2));
+  }
+  return ans;
+}
+
+int main() {
+  vector<int> nums = {-2,0,-1};
+
+  // cout << maxProductBrute(nums) << endl;
+
+  cout << maxProductOptimal_1(nums) << endl;
 }
